27a7245079ce67df0403f115ac0337210ce2e199
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2021-03-22 21:23:50 -0400

    WIP

--- components/Artists.js
+++ components/Artists.js
@@ -1,18 +1,16 @@
+import React from 'react';
+
 class Artists extends React.Component {
-    render() {
-      return (
-            <div className="mt-5">
-                <h3>Artists</h3>
-                {this.props.performers.map(artist => {
-                    return (
-                        <div key={artist.id}>
-                            {artist.title}
-                        </div>
-                    );
-                })}
-            </div>
-      );
-    }
-  }
-  
-  export default Artists;
+	render() {
+		return (
+			<div className="mt-5">
+				<h3>Artists</h3>
+				{this.props.performers.map((artist) => {
+					return <div key={artist.id}>{artist.title}</div>;
+				})}
+			</div>
+		);
+	}
+}
+
+export default Artists;
--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import Layout from '../components/Layout';
 
 const About = () => (
-    <Layout>
-        <div>
-            <h1>About Bits</h1>
-            <p>Our application to view Next.js</p>
-        </div>
-    </Layout>
+	<Layout>
+		<div>
+			<h1>About Bits</h1>
+			<p>Our application to view Next.js</p>
+		</div>
+	</Layout>
 );
 
 export default About;
